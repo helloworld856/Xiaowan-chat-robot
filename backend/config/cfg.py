a478903636048399433c19a32049b4bf2819e9fa
helloworld <3372669346@qq.com>
2026-06-05 09:36:44 +0800

    去除冗余代码

--- backend/config/cfg.py
+++ backend/config/cfg.py
@@ -5,15 +5,6 @@ class Config():
     # 配置根目录的绝对路径
     root_dir_path = dirname(abspath('..config'))
 
-    # 浏览器标签页图标
-    page_icon = join(root_dir_path, 'web', 'page_icon', 'favicon.ico')
-
-    # 网页文件配置
-    web_file_path = join(root_dir_path, 'web/index.py')
-
-    # 用户头像配置
-    user_avatar = join(root_dir_path, 'web/fjnu.png')
-
     # 日志配置
     log_save_path = join(root_dir_path, 'log_config/app.log')
 
@@ -31,6 +22,7 @@ class Config():
     listen_host = "127.0.0.1"
     # listen_host = "0.0.0.0"
     listen_port = 8000
+
 
 configer = Config()
 
